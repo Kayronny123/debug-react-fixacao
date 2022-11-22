7762de7a3726817fca606086b28a8154929fccfb
Rogerio-kayronny-sales <108687095+Kayronny123@users.noreply.github.com>
2022-11-22 13:07:57 -0300

    Initial commit

---
+++ src/components/CardGrande/CardGrande.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  LayoutCardGrande,
+  ImgCardGrande,
+  InfosCardGrande,
+  TituloCardGrande
+} from "./styles";
+
+function CardGrande(props) {
+  return (
+    <LayoutCardGrande>
+      <ImgCardGrande src={props.imagem} />
+      <InfosCardGrande>
+        <TituloCardGrande>{props.nome}</TituloCardGrande>
+        <p>{props.perfil.descricao}</p>
+      </InfosCardGrande>
+    </LayoutCardGrande>
+  );
+}
+export default CardGrande;
---
+++ src/components/CardPequeno/CardPequeno.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  LayoutCardPequeno,
+  TituloCardPequeno,
+  InfosCardPequeno,
+  ImgCardPequeno
+} from "./styles";
+
+function CardPequeno(props) {
+  <LayoutCardPequeno>
+    <ImgCardPequeno src={props.img} />
+    <InfosCardPequeno>
+      <TituloCardPequeno>{props.nome}</TituloCardPequeno>
+      {/* <p>{props.descricao}</p> */}
+    </InfosCardPequeno>
+  </LayoutCardPequeno>;
+}
+
+export default CardPequeno;
